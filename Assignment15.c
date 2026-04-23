d457eaa62b317fc6ac627e1264c706910dc6dade
sannidhishenvi7-eng <sannidhishenvi7@gmail.com>
2026-04-23 19:07:01 +0530

    Update Assignment15.c

--- Assignment15.c
+++ Assignment15.c
@@ -1,17 +1,20 @@
-// Random Numbers
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-int main(){
-    int n,i;
+void main()
+{
+    int i, n;
 
+    printf("How many random numbers do you wantt:");
     scanf("%d",&n);
 
     srand(time(0));
 
-    for(i=0;i<n;i++)
-        printf("%d\n",rand());
+    printf("pseudo random numbers are:\n");
 
-    return 0;
+    for(i = 0; i < n; i++)
+    {
+        printf("%d ",rand());
+    }
 }
