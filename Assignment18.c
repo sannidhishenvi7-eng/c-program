ab3fe2c5c80226176109fd632c2f0f116163c996
sannidhishenvi7-eng <sannidhishenvi7@gmail.com>
2026-04-23 19:14:36 +0530

    string operations into a single main function

--- Assignment18.c
+++ Assignment18.c
@@ -1,86 +1,22 @@
-// Length
 #include <stdio.h>
 #include <string.h>
 
-int main(){
-    char str[100];
-    scanf("%s",str);
-    printf("%lu",strlen(str));
+int main() {
+    char s[100], rev[100], sub[50];
+    printf("Enter string: "); scanf("%s", s);
+    int len = strlen(s);
+    printf("Length: %d\n", len);
+    for(int i=0;i<len;i++) rev[i]=s[len-1-i];
+    rev[len]='\0';
+    printf("Reversed: %s\n", rev);
+    printf("Palindrome: %s\n", strcmp(s,rev)==0?"Yes":"No");
+    printf("Enter substring to search: "); scanf("%s", sub);
+    char *p = strstr(s, sub);
+    if(p) printf("Found at index %d\n", (int)(p-s));
+    else   printf("Not found\n");
     return 0;
 }
 
-
-
-// Reverse
-#include <stdio.h>
-#include <string.h>
-
-int main(){
-    char str[100];
-    scanf("%s",str);
-
-    strrev(str);
-
-    printf("%s",str);
-    return 0;
-}
-
-
-
-// Compare
-#include <stdio.h>
-#include <string.h>
-
-int main(){
-    char a[100],b[100];
-    scanf("%s %s",a,b);
-
-    if(strcmp(a,b)==0)
-        printf("Equal");
-    else
-        printf("Not Equal");
-
-    return 0;
-}
-
-
-
-// Palindrome
-#include <stdio.h>
-#include <string.h>
-
-int main(){
-    char str[100],rev[100];
-    scanf("%s",str);
-
-    strcpy(rev,str);
-    strrev(rev);
-
-    if(strcmp(str,rev)==0)
-        printf("Palindrome");
-    else
-        printf("Not Palindrome");
-
-    return 0;
-}
-
-
-
-// Substring
-#include <stdio.h>
-#include <string.h>
-
-int main(){
-    char str[100],sub[100];
-    scanf("%s %s",str,sub);
-
-    if(strstr(str,sub))
-        printf("Found");
-    else
-        printf("Not Found");
-
-    return 0;
-}
 
 
 
